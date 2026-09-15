4cd73c7151cfb3cf592e4ff60c4489ae42333f1a
carp09-ops <carp09@gmail.com>
2026-09-15 19:39:25 -0400

    Route endurance template to endurance encounter

--- app/workout.tsx
+++ app/workout.tsx
@@ -1,6 +1,10 @@
+import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import ActiveWorkoutV2 from '../src/ActiveWorkoutV2';
+import EnduranceWorkoutV2 from '../src/EnduranceWorkoutV2';
 
 export default function WorkoutScreen() {
+  const params = useLocalSearchParams<{ templateId?: string }>();
+  if (params.templateId === 'run') return <EnduranceWorkoutV2 />;
   return <ActiveWorkoutV2 />;
 }

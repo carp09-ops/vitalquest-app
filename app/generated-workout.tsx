84ba591603691bc7d28cf77be33a2965eb9c10f5
carp09-ops <carp09@gmail.com>
2026-09-16 07:44:49 -0400

    Keep generated workouts inside immersive world

--- app/generated-workout.tsx
+++ app/generated-workout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GeneratedWorkoutV2 from '../src/GeneratedWorkoutV2';
+import WorldBackdrop from '../src/WorldBackdrop';
 
 export default function GeneratedWorkoutScreen(){
-  return <GeneratedWorkoutV2/>;
+  return <WorldBackdrop scene="train"><GeneratedWorkoutV2/></WorldBackdrop>;
 }

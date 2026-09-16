ed3aacfb4b59c5fd87f3fe0232c1ff73c040cece
carp09-ops <carp09@gmail.com>
2026-09-16 07:46:58 -0400

    Keep beta feedback inside immersive world

--- app/beta-feedback.tsx
+++ app/beta-feedback.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
 import BetaFeedbackV2 from '../src/BetaFeedbackV2';
-export default function BetaFeedbackScreen(){return <BetaFeedbackV2/>;}
+import WorldBackdrop from '../src/WorldBackdrop';
+export default function BetaFeedbackScreen(){return <WorldBackdrop scene="hero"><BetaFeedbackV2/></WorldBackdrop>;}

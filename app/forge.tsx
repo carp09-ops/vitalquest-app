43757ec4d6ce10b7e0550837e95183092bea7912
carp09-ops <carp09@gmail.com>
2026-09-16 07:44:42 -0400

    Keep Forge inside immersive world

--- app/forge.tsx
+++ app/forge.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import WorkoutForgeV2 from '../src/WorkoutForgeV2';
+import WorldBackdrop from '../src/WorldBackdrop';
 
 export default function ForgeScreen(){
-  return <WorkoutForgeV2/>;
+  return <WorldBackdrop scene="train"><WorkoutForgeV2/></WorldBackdrop>;
 }

6537cdf9c0fb92432455b8ac54e7f85d4b25630f
carp09-ops <carp09@gmail.com>
2026-09-17 16:23:05 -0400

    Switch onboarding route to visual-contract implementation

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
-import OnboardingV2 from '../src/OnboardingV2';
+import OnboardingV3 from '../src/OnboardingV3';
 
-export default function OnboardingRoute(){return <OnboardingV2/>}
+export default function OnboardingRoute(){return <OnboardingV3/>}

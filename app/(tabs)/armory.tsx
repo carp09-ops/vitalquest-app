d882f42f88d66b6add1584d6c404c6fb60b815e1
carp09-ops <carp09@gmail.com>
2026-09-17 16:43:40 -0400

    Route Rewards to cinematic V3 vault screen

--- app/(tabs)/armory.tsx
+++ app/(tabs)/armory.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ArmoryV2 from '../../src/ArmoryV2';
+import ArmoryV3 from '../../src/ArmoryV3';
 import WorldBackdrop from '../../src/WorldBackdrop';
 
 export default function ArmoryScreen() {
-  return <WorldBackdrop scene="armory"><ArmoryV2 /></WorldBackdrop>;
+  return <WorldBackdrop scene="armory"><ArmoryV3 /></WorldBackdrop>;
 }

ef65a6df307d8043e60c0875c458d72f6c1c2454
carp09-ops <carp09@gmail.com>
2026-09-17 16:42:29 -0400

    Route Goals to cinematic V3 implementation

--- app/(tabs)/quests.tsx
+++ app/(tabs)/quests.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import QuestBoardV2 from '../../src/QuestBoardV2';
+import QuestBoardV3 from '../../src/QuestBoardV3';
 import WorldBackdrop from '../../src/WorldBackdrop';
 
 export default function QuestsScreen() {
-  return <WorldBackdrop scene="quests"><QuestBoardV2 /></WorldBackdrop>;
+  return <WorldBackdrop scene="quests"><QuestBoardV3 /></WorldBackdrop>;
 }

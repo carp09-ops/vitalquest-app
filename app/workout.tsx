f2612d0e0d1316076876a9f6947b4c90116b148a
carp09-ops <carp09@gmail.com>
2026-09-17 16:49:39 -0400

    Route workouts through cinematic V3 encounter shell

--- app/workout.tsx
+++ app/workout.tsx
@@ -1,16 +1,6 @@
-import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import ActiveWorkoutV2 from '../src/ActiveWorkoutV2';
-import EnduranceWorkoutV2 from '../src/EnduranceWorkoutV2';
-import RecoveryEncounterV2 from '../src/RecoveryEncounterV2';
-import WorldBackdrop from '../src/WorldBackdrop';
+import WorkoutExperienceV3 from '../src/WorkoutExperienceV3';
 
-export default function WorkoutScreen() {
-  const params = useLocalSearchParams<{ templateId?: string }>();
-  const encounter = params.templateId === 'run'
-    ? <EnduranceWorkoutV2 />
-    : params.templateId === 'recovery'
-      ? <RecoveryEncounterV2 />
-      : <ActiveWorkoutV2 />;
-  return <WorldBackdrop scene="train">{encounter}</WorldBackdrop>;
+export default function WorkoutScreen(){
+  return <WorkoutExperienceV3/>;
 }
